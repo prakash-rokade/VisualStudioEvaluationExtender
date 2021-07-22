e1fe76c90b046395a256a931641f52767a6060aa
Win10Dev <Win10Dev@DESKTOP-SCSIB17>
2021-07-23 00:38:32 +0530

    Add project files.

---
+++ VisualStudioEvalExtender/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿
+namespace VisualStudioEvalExtender
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnExtend = new System.Windows.Forms.Button();
+            this.dtpExpirationDate = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbVersions = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtMonths = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnExtend
+            // 
+            this.btnExtend.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExtend.Location = new System.Drawing.Point(20, 113);
+            this.btnExtend.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnExtend.Name = "btnExtend";
+            this.btnExtend.Size = new System.Drawing.Size(272, 28);
+            this.btnExtend.TabIndex = 2;
+            this.btnExtend.Text = "Extend";
+            this.btnExtend.UseVisualStyleBackColor = true;
+            this.btnExtend.Click += new System.EventHandler(this.BtnExtend_Click);
+            // 
+            // dtpExpirationDate
+            // 
+            this.dtpExpirationDate.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dtpExpirationDate.CustomFormat = "dd/MM/yyyy";
+            this.dtpExpirationDate.Enabled = false;
+            this.dtpExpirationDate.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpExpirationDate.Location = new System.Drawing.Point(165, 49);
+            this.dtpExpirationDate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dtpExpirationDate.Name = "dtpExpirationDate";
+            this.dtpExpirationDate.Size = new System.Drawing.Size(125, 22);
+            this.dtpExpirationDate.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 17);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Visual Studio Version";
+            // 
+            // cmbVersions
+            // 
+            this.cmbVersions.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmbVersions.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbVersions.FormattingEnabled = true;
+            this.cmbVersions.Items.AddRange(new object[] {
+            "2015",
+            "2017",
+            "2019"});
+            this.cmbVersions.Location = new System.Drawing.Point(165, 14);
+            this.cmbVersions.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cmbVersions.Name = "cmbVersions";
+            this.cmbVersions.Size = new System.Drawing.Size(125, 24);
+            this.cmbVersions.TabIndex = 0;
+            this.cmbVersions.SelectedIndexChanged += new System.EventHandler(this.CmbVersions_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 52);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Expiration Date";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 85);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Extend by ";
+            // 
+            // txtMonths
+            // 
+            this.txtMonths.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtMonths.Location = new System.Drawing.Point(165, 81);
+            this.txtMonths.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtMonths.Name = "txtMonths";
+            this.txtMonths.Size = new System.Drawing.Size(41, 22);
+            this.txtMonths.TabIndex = 7;
+            this.txtMonths.Text = "1";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(216, 85);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Month/s";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(315, 150);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtMonths);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cmbVersions);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dtpExpirationDate);
+            this.Controls.Add(this.btnExtend);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Visual Studio License Extender";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExtend;
+        private System.Windows.Forms.DateTimePicker dtpExpirationDate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbVersions;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtMonths;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ VisualStudioEvalExtender/Form1.cs
@@ -0,0 +1,186 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Windows.Forms;
+
+namespace VisualStudioEvalExtender
+{
+    public partial class Form1 : Form
+    {
+        private readonly Dictionary<string, string> _versionRegSubKeys =
+            new Dictionary<string, string>()
+            {
+                {"2015", @"Licenses\4D8CFBCB-2F6A-4AD2-BABF-10E28F6F2C8F\07078" },
+                {"2017", @"Licenses\5C505A59-E312-4B89-9508-E162F8150517\08878" },
+                {"2019", @"Licenses\41717607-F34E-432C-A138-A3CFD7E25CDA\09278" }
+            };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            TryPopulateInstalledVisualStudioVersions();            
+        }
+
+        private void TryPopulateInstalledVisualStudioVersions()
+        {
+            cmbVersions.Items.Clear();
+            foreach (var item in _versionRegSubKeys)
+            {
+                RegistryKey subKey = Registry.ClassesRoot.OpenSubKey(item.Value, false);
+                if (subKey != null)
+                {
+                    cmbVersions.Items.Add(item.Key);
+                    subKey.Close();
+                }
+            }
+
+            if (cmbVersions.Items.Count < 0)
+            {
+                MessageBox.Show("Visual Studio not installed!!!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+            }
+        }
+
+        private void BtnExtend_Click(object sender, EventArgs e)
+        {
+
+            if(cmbVersions.SelectedIndex < 0 || !int.TryParse(txtMonths.Text, out var result))
+            {
+                MessageBox.Show("Please select Visual Studio version OR enter proper month!!!");
+                return;
+            }
+
+            try
+            {
+                var subKey = _versionRegSubKeys[cmbVersions.SelectedItem.ToString()];
+                SetDate(subKey, result);
+                MessageBox.Show("License extended successfully.", "Done", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                GetDate(subKey);
+            }
+            catch
+            {
+                MessageBox.Show("The license couldn't be extended.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void CmbVersions_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(cmbVersions.SelectedIndex > -1)
+            {
+                GetDate(_versionRegSubKeys[cmbVersions.SelectedItem.ToString()]);
+            }
+        }
+
+        private void GetDate(string regSubKey)
+        {
+            byte[] decryptedData = OpenHKCRSubKey(regSubKey);
+            string expirationDate = ConvertFromBinaryDate(decryptedData);
+
+            dtpExpirationDate.Text = expirationDate;            
+        }
+
+        private void SetDate(string regSubKey, int months)
+        {
+            byte[] decryptedData = OpenHKCRSubKey(regSubKey);
+            DateTime expirationDate = dtpExpirationDate.Value.AddMonths(months);
+            byte[] newData = ConvertToBinaryDate(decryptedData, expirationDate.Year, expirationDate.Month, expirationDate.Day);
+
+            WriteHKCRSubKey(newData, regSubKey);
+        }
+
+        private byte[] OpenHKCRSubKey(string subKey)
+        {
+            RegistryKey regHKCR = Registry.ClassesRoot.OpenSubKey(subKey, true);
+
+            byte[] encryptedData = (byte[])regHKCR.GetValue("", true);
+            byte[] decryptedData = ProtectedData.Unprotect(encryptedData, null, DataProtectionScope.LocalMachine);
+
+            regHKCR.Close();
+
+            return decryptedData;
+        }
+
+        private void WriteHKCRSubKey(byte[] decryptedData, string subKey)
+        {
+            RegistryKey regHKCR = Registry.ClassesRoot.OpenSubKey(subKey, true);
+
+            byte[] encryptedData = ProtectedData.Protect(decryptedData, null, DataProtectionScope.LocalMachine);
+
+            regHKCR.SetValue("", encryptedData);
+            regHKCR.Close();
+        }
+
+        private string ConvertFromBinaryDate(byte[] decryptedData)
+        {
+            // Year
+
+            byte[] YearB = new byte[2];
+            YearB[0] = decryptedData[decryptedData.Length - 15];
+            YearB[1] = decryptedData[decryptedData.Length - 16];
+
+            string YearS = "";
+            YearS += BitConverter.ToString(YearB, 0, 1);
+            YearS += BitConverter.ToString(YearB, 1, 1);
+
+            int YearI = int.Parse(YearS, System.Globalization.NumberStyles.HexNumber);
+
+            // Month
+
+            byte[] MonthB = new byte[2];
+            MonthB[0] = decryptedData[decryptedData.Length - 13];
+            MonthB[1] = decryptedData[decryptedData.Length - 14];
+
+            string MonthS = "";
+            MonthS += BitConverter.ToString(MonthB, 0, 1);
+            MonthS += BitConverter.ToString(MonthB, 1, 1);
+
+            int MonthI = int.Parse(MonthS, System.Globalization.NumberStyles.HexNumber);
+
+            // Day
+
+            byte[] DayB = new byte[2];
+            DayB[0] = decryptedData[decryptedData.Length - 11];
+            DayB[1] = decryptedData[decryptedData.Length - 12];
+
+            string DayS = "";
+            DayS += BitConverter.ToString(DayB, 0, 1);
+            DayS += BitConverter.ToString(DayB, 1, 1);
+
+            int DayI = int.Parse(DayS, System.Globalization.NumberStyles.HexNumber);
+
+            string ExpirationDate = String.Format("{0:00}", DayI) + "/" + String.Format("{0:00}", MonthI) + "/" + YearI.ToString();
+
+            return ExpirationDate;
+        }
+
+        private byte[] ConvertToBinaryDate(byte[] DecryptedData, int Year, int Month, int Day)
+        {
+            // Year
+
+            byte[] YearB = BitConverter.GetBytes(Year);
+            DecryptedData[DecryptedData.Length - 16] = YearB[0];
+            DecryptedData[DecryptedData.Length - 15] = YearB[1];
+
+            // Month
+
+            byte[] MonthB = BitConverter.GetBytes(Month);
+            DecryptedData[DecryptedData.Length - 14] = MonthB[0];
+            DecryptedData[DecryptedData.Length - 13] = MonthB[1];
+
+            // Day
+
+            byte[] DayB = BitConverter.GetBytes(Day);
+            DecryptedData[DecryptedData.Length - 12] = DayB[0];
+            DecryptedData[DecryptedData.Length - 11] = DayB[1];
+
+            return DecryptedData;
+        }
+    }
+}
